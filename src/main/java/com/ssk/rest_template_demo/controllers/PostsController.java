329db74abe5046e940752ef91cc220ba4ad3105e
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-11 10:50:45 +0530

    added getForEntity api with Object... uriVariables

--- src/main/java/com/ssk/rest_template_demo/controllers/PostsController.java
+++ src/main/java/com/ssk/rest_template_demo/controllers/PostsController.java
@@ -1,15 +1,30 @@
 package com.ssk.rest_template_demo.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.ssk.rest_template_demo.models.Posts;
 
 @RestController
 @RequestMapping("/api")
 public class PostsController {
 
+	RestTemplate restTemplate = new RestTemplate();
+
 	@GetMapping("/testing")
 	public String testController() {
 		return "This is Posts Controller...";
+	}
+
+	@GetMapping("/posts/{id}")
+	public Posts getPost(@PathVariable int id) {
+		ResponseEntity<Posts> responseEntity = restTemplate
+				.getForEntity("https://jsonplaceholder.typicode.com/posts/{id}", Posts.class, id);
+
+		return responseEntity.getBody();
 	}
 }
