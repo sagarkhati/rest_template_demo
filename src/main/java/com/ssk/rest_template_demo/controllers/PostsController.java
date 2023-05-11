03d2b227caf9233810725521e44d80a809103c77
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-05-11 10:53:35 +0530

    added getForEntity api with Map<String,?> uriVariables

--- src/main/java/com/ssk/rest_template_demo/controllers/PostsController.java
+++ src/main/java/com/ssk/rest_template_demo/controllers/PostsController.java
@@ -1,5 +1,8 @@
 package com.ssk.rest_template_demo.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +27,17 @@ public class PostsController {
 	public Posts getPost(@PathVariable int id) {
 		ResponseEntity<Posts> responseEntity = restTemplate
 				.getForEntity("https://jsonplaceholder.typicode.com/posts/{id}", Posts.class, id);
+
+		return responseEntity.getBody();
+	}
+
+	@GetMapping("/posts-using-map/{id}")
+	public Posts getPostUsingMap(@PathVariable int id) {
+		Map<String, Integer> uriVariables = new HashMap<String, Integer>();
+		uriVariables.put("id", id);
+
+		ResponseEntity<Posts> responseEntity = restTemplate
+				.getForEntity("https://jsonplaceholder.typicode.com/posts/{id}", Posts.class, uriVariables);
 
 		return responseEntity.getBody();
 	}
